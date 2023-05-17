0c229188ec6d0028c5f2677aab070e3d634a1ca2
ssshenjy <ssshenjy@126.com>
2023-05-17 14:13:10 -0700

    Sally's chart and chart description

--- Sally's chart.R
+++ Sally's chart.R
@@ -5,21 +5,21 @@ state_vacc <- read.csv("us_state_vaccinations.csv", stringsAsFactors = FALSE)
 state_vacc$date <- as.Date(state_vacc$date)
 state_selected <- c("California", "American Samoa", "Washington", "New York State", "Pennsylvania")
 
-state_filter <- state_vacc %>% 
+state_data_filter <- state_vacc %>% 
   filter(location %in% state_selected & !is.na(total_vaccinations)) %>% 
   group_by(location, date)
 
-ggplot(state_filter, aes(x = date, y = total_vaccinations, color = location)) +
+ggplot(state_data_filter, aes(x = date, y = total_vaccinations, color = location)) +
   geom_line() +
-  labs(title = "Total Vaccinations",
-       x = "Date",
+  labs(title = "Total Vaccinations of COVID-19 in selected states from 2021 to 2023",
+       x = "Year",
        y = "Total Vaccinations") +
-  scale_x_date(date_labels = "%Y-%m", date_breaks = "6 months") +
-  scale_color_manual(values = c("California" = "red", "American Samoa" = "yellow", "Washington" = "blue", "New York State" = "green", "Pennsylvania" = "cyan"))
+  scale_x_date(date_labels = "%Y", date_breaks = "1 year") +
+  scale_color_manual(values = c("California" = "pink", "American Samoa" = "yellow", "Washington" = "blue", "New York State" = "green", "Pennsylvania" = "cyan"))
 
 
 # highest case: California --> 11983239 cases
 # lowest case: American Samoa
 # Washington: earliest case state
 # Most deaths: New York City, New York
-# sate highest ratio(death/case): Pennsylvania
+# state highest ratio(death/case): Pennsylvania
