26d8ff302de2fc6583e45fd029ed6345fc5f68a8
ssshenjy <ssshenjy@126.com>
2023-05-17 20:17:19 -0700

    Chart descirption done

--- Sally's chart.R
+++ Sally's chart.R
@@ -1,5 +1,7 @@
 library(ggplot2)
 library(dplyr)
+library(scales)
+
 
 state_vacc <- read.csv("us_state_vaccinations.csv", stringsAsFactors = FALSE)
 state_vacc$date <- as.Date(state_vacc$date)
@@ -9,11 +11,23 @@ state_data_filter <- state_vacc %>%
   filter(location %in% state_selected & !is.na(total_vaccinations)) %>% 
   group_by(location, date)
 
+ggplot(state_data_filter) +
+  geom_line(aes(x = date, y = total_vaccinations, color = location)) +
+  labs(title = "Total Vaccinations of COVID-19 in selected states from 2021 to 2023",
+       x = "Year",
+       y = "Total Vaccinations",
+       color = "States") +
+  scale_x_date(date_labels = "%Y", date_breaks = "year") +
+  scale_y_continuous(labels = label_number_si()) +
+  scale_color_brewer(palette = "Set2")
+
+
 ggplot(state_data_filter, aes(x = date, y = total_vaccinations, color = location)) +
   geom_line() +
   labs(title = "Total Vaccinations of COVID-19 in selected states from 2021 to 2023",
        x = "Year",
-       y = "Total Vaccinations") +
+       y = "Total Vaccinations",
+       color = "States") +
   scale_x_date(date_labels = "%Y", date_breaks = "1 year") +
   scale_color_manual(values = c("California" = "pink", "American Samoa" = "yellow", "Washington" = "blue", "New York State" = "green", "Pennsylvania" = "cyan"))
 
