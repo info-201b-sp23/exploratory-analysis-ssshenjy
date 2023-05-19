004589f836fe51e2a8317cdc165e3abf17c42820
candiiiceH <zh365@uw.edu>
2023-05-18 22:58:25 -0700

    table edited

--- table.R
+++ table.R
@@ -2,6 +2,8 @@ library(ggplot2)
 library(dplyr)
 library(scales)
 library(tidyverse)
+library(knitr)
+library(kableExtra)
 # Read the dataset
 state_vacc <- read_csv("us_state_vaccinations.csv")
 
@@ -11,8 +13,6 @@ aggregate_data <- state_vacc %>%
   group_by(location) %>% 
   select(date, location, total_vaccinations, people_fully_vaccinated, share_doses_used) %>%
   group_by(date) %>% 
-  summarize("Average Total Doses Administered " = mean(total_vaccinations, na.rm = TRUE), 
+  summarize("Average Total Doses Administered" = mean(total_vaccinations, na.rm = TRUE), 
             "Average People Fully Vaccinated" = mean(people_fully_vaccinated, na.rm = TRUE),
-            "Average Shared Doses Used" = mean(share_doses_used, na.rm = TRUE))
-
-print(aggregate_data)
+            "Average Shared Doses Used" = round(mean(share_doses_used, na.rm = TRUE), digits = 2))
